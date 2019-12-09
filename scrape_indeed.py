d5c676dc2c47869902afe0595dc7ab575be0184a
dundarkarabay <53024047+dundarkarabay@users.noreply.github.com>
2019-12-09 13:51:12 -0600

    commit

--- scrape_indeed.py
+++ scrape_indeed.py
@@ -11,6 +11,9 @@ import requests
 import pymongo
 import pandas as pd
 import time
+import os
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 
 
 # In[2]:
@@ -35,8 +38,18 @@ import time
 
 #trying Splinter
 def init_browser():
-    executable_path = {"executable_path": "chromedriver"}
-    return Browser('chrome', **executable_path, headless=False)
+    options = Options()
+
+    options.binary_location = os.environ.get('GOOGLE_CHROME_BIN')
+
+    options.add_argument('--headless')
+    options.add_argument('--disable-gpu')
+    options.add_argument('--no-sandbox')
+    options.add_argument('--remote-debugging-port=9222')
+    
+    return webdriver.Chrome(executable_path=str(os.environ.get('CHROMEDRIVER_PATH')), chrome_options=options)
+    # executable_path = {"executable_path": "chromedriver"}
+    # return Browser('chrome', **executable_path, headless=False)
 
 # In[27]:
 
