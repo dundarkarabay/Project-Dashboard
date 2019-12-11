ecc7722fa855b04f652c453e25c19ea2ad7ebb52
dundarkarabay <53024047+dundarkarabay@users.noreply.github.com>
2019-12-11 17:35:15 -0600

    commit

--- app.py
+++ app.py
@@ -3,16 +3,16 @@ from flask_pymongo import PyMongo
 import scrape_indeed
 import csv
 import os
-from datetime import datetime
-now = datetime.now()
-current_time = now. strftime("%D/%H/%M/%S")
-db_name = f"dundar/{current_time}"
-db_name = db_name.replace("/", "_")
+# from datetime import datetime
+# now = datetime.now()
+# current_time = now. strftime("%D/%H/%M/%S")
+# db_name = f"job/app/{current_time}"
+# db_name = db_name.replace("/", "_")
 # Create an instance of Flask
 app = Flask(__name__)
 
 # Use PyMongo to establish Mongo connection
-mongo = PyMongo(app, uri=f"mongodb://dundarkarabay:12Subat07@cluster0-shard-00-00-7agox.mongodb.net:27017,cluster0-shard-00-01-7agox.mongodb.net:27017,cluster0-shard-00-02-7agox.mongodb.net:27017/{db_name}?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
+mongo = PyMongo(app, uri=f"mongodb://dundarkarabay:12Subat07@cluster0-shard-00-00-7agox.mongodb.net:27017,cluster0-shard-00-01-7agox.mongodb.net:27017,cluster0-shard-00-02-7agox.mongodb.net:27017/job_relocation?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority")
 
 # Route to render index.html template using data from Mongo
 @app.route("/")
@@ -42,29 +42,34 @@ def thuria():
 # Route that will trigger the scrape function
 @app.route("/scrape")
 def scrape():
+    try:
+        # Run the scrape function
+        scraping_data = scrape_indeed.scrape_info()
 
-    # Run the scrape function
-    scraping_data = scrape_indeed.scrape_info()
+        # Update the Mongo database using update and upsert=True
+        mongo.db.collection.insert(scraping_data)
 
-    # Update the Mongo database using update and upsert=True
-    mongo.db.collection.insert(scraping_data)
+        # for data in scraping_data:
+        #     print(data["location"])
+        titles = []
+        locations = []
+        metadata = {}
+        for data in scraping_data:
+            titles.append(data["title"])
+            locations.append(data["location"])
+        
+        metadata["title"] = titles
+        metadata["location"] = locations
 
-    # for data in scraping_data:
-    #     print(data["location"])
-    titles = []
-    locations = []
-    metadata = {}
-    for data in scraping_data:
-        titles.append(data["title"])
-        locations.append(data["location"])
-    
-    metadata["title"] = titles
-    metadata["location"] = locations
+        # print(metadata)
+        # return jsonify(metadata)
+        # Redirect back to home page
+        return redirect("/")
+    except Exception as e:
+        errorMessage = {}
+        errorMessage["Message"] = "Please go back to the homepage and keep trying until scraping is successfully executed!"
+        return jsonify(errorMessage)
 
-    print(metadata)
-    return jsonify(metadata)
-    # Redirect back to home page
-    # return redirect("/")
 
 if __name__ == "__main__":
     app.run(debug=True)
